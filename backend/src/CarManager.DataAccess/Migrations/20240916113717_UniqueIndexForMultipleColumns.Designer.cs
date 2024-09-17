656a7e165bb9d4b77768e2ad685b1990f737e91d
kirillezhov <kirillezhov154@gmail.com>
2024-09-17 12:50:10 +0300

    Backend: add unique index for multiple columns

--- backend/src/CarManager.DataAccess/Migrations/20240916113717_UniqueIndexForMultipleColumns.Designer.cs
+++ backend/src/CarManager.DataAccess/Migrations/20240916113717_UniqueIndexForMultipleColumns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarManager.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarManager.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240916113717_UniqueIndexForMultipleColumns")]
+    partial class UniqueIndexForMultipleColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
