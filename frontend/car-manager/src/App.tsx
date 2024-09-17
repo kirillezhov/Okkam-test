35a05f7eb79ce4871d3d14254de0f24a158e9892
kirillezhov <kirillezhov154@gmail.com>
2024-09-17 12:53:19 +0300

    Frontend: apply NewCarModal

--- frontend/car-manager/src/App.tsx
+++ frontend/car-manager/src/App.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from 'react';
 import { Button, Flex, Table, TableColumnsType } from 'antd';
-import { useEffect, useState } from "react";
-import { Car } from "./models/Car.ts";
-import axios from "axios";
+import moment from 'moment';
+
+import axiosInstance from './utils/axiosInstance.ts';
+import { Car } from './models/Car.ts';
+import NewCarModal from './components/NewCarModal';
+import { BodyType } from './models/BodyType.ts';
+import { Brand } from './models/Brand.ts';
 
 interface DataType {
     key: React.Key,
@@ -20,33 +25,55 @@ const columns: TableColumnsType<DataType> = [
     { title: 'Количество сидений', dataIndex: 'seatsCount' },
     { title: 'Изображение', dataIndex: 'key', render: (id: string) => <a href={`http://localhost:5206/api/cars/image/${id}`} download>Download Image</a> },
     { title: 'Ссылка', dataIndex: 'url', render: (url: string) => <a href={url} target="_blank">{url}</a> },
-    { title: 'Создан', dataIndex: 'createdAt' }
+    { title: 'Создан', dataIndex: 'createdAt', render: (date: Date) => (moment(date)).format('DD-MMM-YYYY HH:mm:ss') },
 ];
 
 const App = () =>  {
-    const [data, setData] = useState<Car[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+    const [cars, setCars] = useState<Car[]>([]);
+    const [bodyTypes, setBodyTypes] = useState<BodyType[]>([]);
+    const [brands, setBrands] = useState<Brand[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
+    const [newCarModalVisible, setNewCarModalVisible] = useState<boolean>(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get<Car[]>('http://localhost:5206/api/cars', {
-                    headers: {"Access-Control-Allow-Origin": "*"}
-                });
-                setData(response.data);
-            } catch (err) {
-                console.log(err);
-                setError('Ошибка при загрузке данных');
-            } finally {
-                setLoading(false);
-            }
-        };
+    const fetchCars = async () => {
+        setLoading(true);
 
-        fetchData();
-    }, []);
+        try {
+            const response = await axiosInstance.get<Car[]>('/cars');
 
-    const dataSource = data.map<DataType>((car) => ({
+            setCars(response.data);
+        } catch (err) {
+            console.log(err);
+            setError('Ошибка при загрузке данных');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchBodyTypes = async () => {
+        try {
+            const response = await axiosInstance.get<BodyType[]>('/bodyTypes');
+
+            setBodyTypes(response.data);
+        } catch (err) {
+            console.log(err);
+            setError('Ошибка при загрузке справочника BodyTypes');
+        }
+    };
+
+    const fetchBrands = async () => {
+        try {
+            const response = await axiosInstance.get<Brand[]>('/brands');
+
+            setBrands(response.data);
+        } catch (err) {
+            console.log(err);
+            setError('Ошибка при загрузке справочника Brands');
+        }
+    };
+
+    const dataSource = cars.map<DataType>((car) => ({
         key: car.id,
         bodyType: car.bodyType.name,
         brand: car.brand.name,
@@ -56,14 +83,36 @@ const App = () =>  {
         createdAt: car.createdAt
     }));
 
+    const handleOnCancelModal = async () => {
+        setNewCarModalVisible(false);
+        await fetchCars();
+    }
+
+    useEffect(() => {
+        fetchCars();
+        fetchBodyTypes();
+        fetchBrands();
+    }, []);
+
     return (
         <Flex gap="middle" vertical>
             <Flex align="center" gap="middle">
-                <Button type="primary">Новый авто</Button>
+                <Button type="primary" onClick={() => setNewCarModalVisible(true)}>Новый автомобиль</Button>
             </Flex>
-            <Table columns={columns} dataSource={dataSource} rowKey="id" />
-            {loading && <div>Загрузка...</div>}
-            {error && <div>Ошибка: {error}</div>}
+            <Table
+                columns={columns}
+                dataSource={dataSource}
+                rowKey="key"
+                loading={loading}
+                footer={() => error && <div>Ошибка: {error}</div>}
+                pagination={{ pageSize: 10 }}
+            />
+            <NewCarModal
+                visible={newCarModalVisible}
+                onCancel={handleOnCancelModal}
+                brands={brands}
+                bodyTypes={bodyTypes}
+            />
         </Flex>
     );
 };
